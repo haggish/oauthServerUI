07edbd91e75198ffc9f8115e70279ab21b1e356c
Mika Salminen <mika.o.salminen@gmail.com>
2013-09-05 21:42:48 +0200

    Integration of AngularJS and Mongolab to token list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,11 +3,16 @@
 /* Controllers */
 
 angular.module('oauthServerUI.controllers', []).
-    controller('Tokens', ['$scope', 'tokens',
-        function ($scope, tokens) {
-
+    controller('Tokens', ['$scope', 'tokens', 'resources',
+        function ($scope, tokens, resources) {
+            $scope.resourcesOfUser = resources.query(); // FIXME: only resources of cuser
+            // FIXME: see what is selected in resources, then query their scopes, then union
+            $scope.unionOfResourceScopes = [ 'read', 'write', 'delete' ];
+            // FIXME: same for authzd grant types of resources
+            $scope.unionOfAuthorizedGrantTypes = [ 'AUTHORIZATION_CODE', 'IMPLICIT' ];
+            $scope.tokens = tokens.query();
         }])
     .controller('Resources', ['$scope', 'resources',
         function ($scope, resources) {
-
+            $scope.resources = resources.query();
         }]);
