b00d64f20abf713e67c1298f77cb705918ab6a6d
Mika Salminen <mika.o.salminen@gmail.com>
2013-09-06 17:12:57 +0200

    Implemented bidir binding directives; not sure if they work 100% yet

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,65 @@
 
 /* Directives */
 
-
-angular.module('oauthServerUI.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+var binder = function (parser, formatter) {
+    return function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModel) {
+                ngModel.$parsers.push(parser);
+                ngModel.$formatters.push(formatter);
+            }
+        };
     };
-  }]);
+};
+
+angular.module('oauthServerUI.directives', [])
+    .directive('comma-list-string-to-array-binding', binder(
+        function (stringArray) {
+            if (!stringArray || stringArray.length == 0) {
+                return '';
+            }
+            var list = '';
+            for (var i = 0; i < stringArray.length; i++) {
+                if (list !== '') {
+                    list += ', ';
+                }
+                list += stringArray[i];
+            }
+            return list;
+        },
+        function (text) {
+            if (!text) {
+                return [];
+            }
+            return text.split(',').map(function (e) {
+                return e.trim();
+            });
+        }
+    ))
+    .directive('prop-eq-val-lines-to-obj-binding', binder(
+        function (object) {
+            if (!object) {
+                return '';
+            }
+            var out = '';
+            for (var property in object) {
+                out += (property + ' = ' + object[property] + '\n');
+            }
+            return out;
+        },
+        function (text) {
+            if (!text) {
+                return {};
+            }
+            var out = {};
+            var lines = text.split('\n');
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                var propAndValue = line.split('=');
+                out[propAndValue[0]] = propAndValue[1];
+            }
+            return out;
+        }
+    ));
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,31 +2,4 @@
 
 /* Filters */
 
-angular.module('oauthServerUI.filters', [])
-    .filter('commaSeparated', function () {
-        return function (stringArray) {
-            if (!stringArray || stringArray.length == 0) {
-                return '';
-            }
-            var list = '';
-            for (var i = 0; i < stringArray.length; i++) {
-                if (list !== '') {
-                    list += ', ';
-                }
-                list += stringArray[i];
-            }
-            return list;
-        };
-    })
-    .filter('propertyLined', function () {
-        return function (object) {
-            if (!object) {
-                return '';
-            }
-            var out = '';
-            for (var property in object) {
-                out += (property + ' = ' + object[property] + '\n');
-            }
-            return out;
-        }
-    });
+angular.module('oauthServerUI.filters', []);
